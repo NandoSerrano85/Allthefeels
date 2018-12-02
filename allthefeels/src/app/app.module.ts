04c41d91e32edacd905bd02505049fb5206c0596
Alejandra Massuh <mmass004@fiu.edu>
2018-12-01 21:34:36 -0500

    trying to add navbar with boostrap

--- allthefeels/src/app/app.module.ts
+++ allthefeels/src/app/app.module.ts
@@ -3,16 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './core/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, NavbarComponent]
 })
 export class AppModule { }
---
+++ allthefeels/src/app/core/navbar.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'ct-navbar',
+    templateUrl: 'navbar.component.html',
+    host: {
+      '(document:click)': 'closeAll()'
+    }
+})
+export class NavbarComponent {
+    menuIsOpen = false;
+    dropdownIsOpen = false;
+    toggleMenu(e: Event) {
+      let bool = this.menuIsOpen;
+      this.menuIsOpen = bool === false ? true : false; 
+      e.stopPropagation();
+    }
+    toggleDropdown(e: Event) {
+      let bool = this.dropdownIsOpen;
+      this.dropdownIsOpen = bool === false ? true : false;
+      e.stopPropagation();
+    }
+    closeAll() {
+      setTimeout(() => {
+        this.dropdownIsOpen = false;
+        this.menuIsOpen = false;
+      }, 200);
+    }
+} 
